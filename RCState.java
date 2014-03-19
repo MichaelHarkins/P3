d6ab202da41a637affb192deb88387061b0f9b66
Brent <breinhardt319@gmail.com>
2014-03-18 23:29:12 -0400

    commit

--- RCState.java
+++ RCState.java
@@ -6,65 +6,14 @@ import edu.cwru.sepia.environment.model.state.Unit.UnitView;
 
 
 public class RCState {
-private int gCost;
-public int getgCost() {
-	return gCost;
-}
-
-public void setgCost(int gCost) {
-	this.gCost = gCost;
-}
-
-public int getfCost() {
-	return fCost;
-}
-
-public void setfCost(int fCost) {
-	this.fCost = fCost;
-}
-
-public int gethCost() {
-	return hCost;
-}
-
-public void sethCost(int hCost) {
-	this.hCost = hCost;
-}
-
-public List<RCState> getNeighbors() {
-	return neighbors;
-}
-
-public void setNeighbors(List<RCState> neighbors) {
-	this.neighbors = neighbors;
-}
-
-public List<String> getState() {
-	return state;
-}
-
-public void setState(List<String> state) {
-	this.state = state;
-}
-private int fCost;
-private int hCost;
-
-private List<RCState> neighbors = new LinkedList<RCState>();
 private List<String> state = new LinkedList<String>();
 
-public RCState(List<String> state, int gCost, int hCost){
+public RCState(List<String> state){
 	for(String s: state){
 		state.add(s);
 	}
-	this.gCost = gCost;
-	this.hCost = hCost;
-	fCost = gCost + hCost;
 }
 
-public RCState clone(){
-	RCState c = new RCState(state,getgCost(),gethCost());
-	return c;
-}
 public boolean Peasant(String peasant){
 	return state.contains("Peasant(" + peasant + ")");
 }
@@ -181,8 +130,6 @@ public boolean GoNear(String peasant, String unit){
 	if(Peasant(peasant)){
 	int index = -1;
 	for(String s: state){
-		if(s.equalsIgnoreCase("Near(" + unit + "," + peasant + ")"))
-			return false;
 		if(s.contains("Near("))
 			index = state.indexOf(s);
 	}
